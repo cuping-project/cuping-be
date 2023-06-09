607673cf74023cfeb95b107d72b90bc92d3763a3
Jangkiwoong <zoo11111@gmail.com>
2023-06-10 05:16:51 +0900

    Revert "Revert "[Refactor] : 카페 이미지 추가""
    This reverts commit 040b38d9c852945986d204a39fe2508ece3794de.

--- src/main/java/com/cuping/cupingbe/dto/OwnerPageRequestDto.java
+++ src/main/java/com/cuping/cupingbe/dto/OwnerPageRequestDto.java
@@ -2,16 +2,26 @@ package com.cuping.cupingbe.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class OwnerPageRequestDto {
 
     private String storeName;
     private String storeAddress;
     private String storeNumber;
     private MultipartFile authImage;
+    private MultipartFile cafeImage;
+
+    public OwnerPageRequestDto(String storeName, String storeAddress, String storeNumber, MultipartFile authImage) {
+        this.storeName = storeName;
+        this.storeAddress = storeAddress;
+        this.storeNumber = storeNumber;
+        this.authImage = authImage;
+    }
 }
