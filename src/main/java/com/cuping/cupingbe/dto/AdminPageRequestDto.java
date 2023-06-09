fb5f65ee062542337579d5015d242d515dee5cbd
Jangkiwoong <zoo11111@gmail.com>
2023-06-09 16:42:57 +0900

    [Feat] : adminPage TestCode작성(승인되지 않은 카페 조회, 원두 생성)

--- src/main/java/com/cuping/cupingbe/dto/AdminPageRequestDto.java
+++ src/main/java/com/cuping/cupingbe/dto/AdminPageRequestDto.java
@@ -2,9 +2,11 @@ package com.cuping.cupingbe.dto;
 
 import jakarta.persistence.Column;
 import lombok.Data;
+import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
+@Setter
 public class AdminPageRequestDto {
     private MultipartFile image;
     private String beanName;
