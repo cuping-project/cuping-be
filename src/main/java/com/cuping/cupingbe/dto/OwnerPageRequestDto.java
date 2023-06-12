597360f9f7701778ecb4670d5fe562cb3a77273d
Jangkiwoong <zoo11111@gmail.com>
2023-06-12 13:01:56 +0900

    회원가입 카페 이미지 제외

--- src/main/java/com/cuping/cupingbe/dto/OwnerPageRequestDto.java
+++ src/main/java/com/cuping/cupingbe/dto/OwnerPageRequestDto.java
@@ -18,10 +18,4 @@ public class OwnerPageRequestDto {
     private MultipartFile authImage;
     private MultipartFile cafeImage;
 
-    public OwnerPageRequestDto(String storeName, String storeAddress, String storeNumber, MultipartFile authImage) {
-        this.storeName = storeName;
-        this.storeAddress = storeAddress;
-        this.storeNumber = storeNumber;
-        this.authImage = authImage;
-    }
 }
