ceeb0c1217a418c75e1e9e4d2274250a840057a7
JEONGHUNJEON <jjhoon06@gmail.com>
2023-06-20 22:46:07 +0900

    [Feat] queryDsl 초기 세팅

--- src/main/java/com/cuping/cupingbe/repository/UserRepository.java
+++ src/main/java/com/cuping/cupingbe/repository/UserRepository.java
@@ -2,11 +2,12 @@ package com.cuping.cupingbe.repository;
 
 import java.util.Optional;
 
+import com.cuping.cupingbe.repository.queryDsl.CustomUserRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cuping.cupingbe.entity.User;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,Long>, CustomUserRepository {
 	Optional<User> findByUserId(String userId);
 	Optional<User> findByNickname(String nickname);
 
