2a24a85d65bb782190912bd5338c140f081c816f
Jangkiwoong <zoo11111@gmail.com>
2023-06-21 16:49:21 +0900

    [Feat] : queryDSL 생성중

--- src/main/java/com/cuping/cupingbe/repository/queryDsl/CustomCommentRepositoryImpl.java
+++ src/main/java/com/cuping/cupingbe/repository/queryDsl/CustomCommentRepositoryImpl.java
@@ -1,10 +1,26 @@
 package com.cuping.cupingbe.repository.queryDsl;
 
+import com.cuping.cupingbe.entity.Bean;
+import com.cuping.cupingbe.entity.Comment;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import static com.cuping.cupingbe.entity.QComment.comment;
+
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class CustomCommentRepositoryImpl implements CustomCommentRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public Optional<Comment> findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Comment> findByBean(Bean bean) {
+        return null;
+    }
 }
